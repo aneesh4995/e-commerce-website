ca9df0e4d1799581fa934cd9fab8598a974429b6
aneesh4995 <smullap3@kent.edu>
2020-01-18 13:38:43 -0500

    added redux to save state of quantity

--- src/components /collection-item/collection-item.component.jsx
+++ src/components /collection-item/collection-item.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { addItem } from '../../redux/cart/cart.actions'
+
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
 
-const CollectionItem = ({id, imageUrl, name, price}) => (
+const CollectionItem = ({ item, addItem}) => {
+    const { imageUrl, name, price } = item;
+    return (
     <div className = 'collection-item'>
         <div
         className ='image'
@@ -14,7 +20,18 @@ const CollectionItem = ({id, imageUrl, name, price}) => (
             <span className='name'>{name}</span>
             <span className='price'>${price}</span>
         </div>
-    </div>
-);
+        <CustomButton onClick={() => addItem(item)} inverted> 
+            ADD TO CART
+        </CustomButton>
+        </div>
+    );
+};
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+    null, 
+    mapDispatchToProps
+    )(CollectionItem);
