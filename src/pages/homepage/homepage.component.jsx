ab4b611ccf7fc73388c7a1949027fbc9c3de9590
aneesh4995 <smullap3@kent.edu>
2020-01-20 13:12:36 -0500

    added react rewired to eliminate out of source error

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Directory from '/home/aneesh/Desktop/ecomm/crwn-app/src/components /directory/directory.component.jsx'
+import Directory from '../../components /directory/directory.component'
 import'./homepage.styles.scss';
 
 const HomePage = () => (
