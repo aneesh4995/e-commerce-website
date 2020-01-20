fbf273387c068b88000e298a99058a704f401c68
aneesh4995 <smullap3@kent.edu>
2020-01-20 12:33:23 -0500

    added favicon stripe payment and title changed

--- src/components /collections-overview/collection-overview.componet.jsx
+++ src/components /collections-overview/collection-overview.componet.jsx
@@ -1,22 +1,22 @@
 import React from'react';
 import {connect } from 'react-redux';
-import { selectCollections } from '../../redux/shop/shop.selector';
 
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview  from '../preview-collection /preview-collection.component'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import './collection-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className = 'collections-overview'>
-        {collections.map(({ id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/> 
-        ))}
+    <div className='collections-overview'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
     </div>
-);
-
-const mapStateToProps = createStructuredSelector({
-    collections:  selectCollections
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+  );
+  
+  const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+  });
+  
+  export default connect(mapStateToProps)(CollectionsOverview);
